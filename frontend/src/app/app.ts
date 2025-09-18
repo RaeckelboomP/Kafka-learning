a455f4f8c32a460e1ed5144e83a9da6563389ef5
RaeckelboomP <pierre.raeckelboom@gmail.com>
2025-09-18 18:57:18 +0000

    feat: Satisfying local application.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,20 +1,38 @@
 import { Component, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('kafka-frontend');
   webSocketConnected = true;
-  messageInput = '';
   items = [
-  {message: 'Sword'},
-  {message: 'Potion'},
-  {message: 'Shield'},
+  {message: 'Sword', topic: 'topic-1'},
+  {message: 'Potion', topic: 'topic-1'},
+  {message: 'Shield', topic: 'topic-2'},
   ];
+
+  get topic1Items() {
+  return this.items.filter(i => i.topic === 'topic-1');
+}
+  get topic2Items() {
+  return this.items.filter(i => i.topic === 'topic-2');
+}
+
+  messageForm: FormGroup = new FormGroup({
+    message: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] })
+  });
+
+  sendMessage(topic: string) {
+    const message = this.messageForm.value.message?.trim();
+    if (!message) return;
+    console.log(`Sending "${message}" to ${topic}`);
+    this.items.push({message, topic});
+    this.messageForm.setValue({ message: '' });
+  };
 }
